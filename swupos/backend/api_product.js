a2b88232e8c283c000870ac5bb65fe27c6086989
Cherrystz <pharuthapol.junpet@g.swu.ac.th>
2021-05-19 20:32:49 +0700

    day 3

--- swupos/backend/api_product.js
+++ swupos/backend/api_product.js
@@ -14,8 +14,29 @@ router.post("/product", (req, res) => {
   form.parse(req, async (error, fields, files) => {
     const doc = await Products.create(fields);
     await uploadImage(files, doc);
-    res.json({ result: "ok" });
+    res.json({ result: "ok", doc });
   });
+});
+
+router.put("/product", async (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+    console.log(JSON.stringify({ err, fields, files }));
+    await Products.findOneAndUpdate({ product_id: fields.product_id }, fields);
+    await uploadImage(files, fields);
+    res.json({ result: "ok", message: fields });
+  });
+});
+
+
+router.delete("/product/id/:id", async (req, res) => {
+  let doc = await Products.findOneAndDelete({ product_id: req.params.id });
+  res.json({ result: "ok", message: doc });
+});
+
+router.get("/product/id/:id", async (req, res) => {
+  let doc = await Products.findOne({ product_id: req.params.id });
+  res.json(doc);
 });
 
 const uploadImage = async (files, doc) => {
