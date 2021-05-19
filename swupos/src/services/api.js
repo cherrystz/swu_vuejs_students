7601c86dda00bf0c19388b507e210c9bfb9a2d80
Cherrystz <pharuthapol.junpet@g.swu.ac.th>
2021-05-19 09:38:41 +0700

    day 3

--- swupos/src/services/api.js
+++ swupos/src/services/api.js
@@ -1,12 +1,18 @@
+import httpClient from "@/services/httpClient";
 import { server } from "@/services/constants";
+import router from "@/router";
+
 const axios = require('axios');
 
 const register =  async(account) => {
-  const result = await axios.post(
-    "http://localhost:3000/api/v2/register",
-    account
-  );
-  return result.data
-}
+  const result = await httpClient.post(server.REGISTER_URL, values);
+  if (result.data.result == "ok") {
+    router.push("login");
+    return true;
+  }
+  else {
+    return false;
+  }
+};
 
 export default { register };
