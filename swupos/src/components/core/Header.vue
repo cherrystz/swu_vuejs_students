d7087abf7d02ad680bae70130047384e72417be9
Cherrystz <pharuthapol.junpet@g.swu.ac.th>
2021-05-19 13:42:46 +0700

    day 3

--- swupos/src/components/core/Header.vue
+++ swupos/src/components/core/Header.vue
@@ -1,13 +1,13 @@
 <template lang="html">
   <v-app-bar app color="#1c2835" dark>
-    <v-btn v-if="true" icon>
+    <v-btn  v-if="isShowBackBtn" icon>
       <v-icon>mdi-arrow-left</v-icon>
     </v-btn>
     <v-toolbar-title>CMPOS Workshop V</v-toolbar-title>
     <v-spacer></v-spacer>
     <h1>Dev  {{ $store.state.count }}</h1>
     <!-- <span>{{ $store.getters["username"] | capitalize }}</span> -->
-    <v-btn icon>
+    <v-btn icon @click="onClickLogOff">
       <v-icon>mdi-export</v-icon>
     </v-btn>
   </v-app-bar>
@@ -22,8 +22,14 @@ const sub_paths = [
     name: 'Header',
     props: [],
     computed: {
-      random() {
-        return Math.random()
+     version(){
+        return process.env.VUE_APP_VERSION
+      },
+      isShowBackBtn(){
+        return this.$route.matched.some(({ name }) => {
+          let index = sub_paths.indexOf(name)
+          return index != -1
+          })
       }
     },
     methods: {
